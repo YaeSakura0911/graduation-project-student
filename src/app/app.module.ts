05a4b82d2f8dd1601d7a62251bae9a61f9b9749a
Rayark-VOEZ <jiangkaiwen0911@gmail.com>
2022-01-12 10:57:41 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+
+// 布局
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+// 导航
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+// 数据展示
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+
+registerLocaleData(zh);
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        NzGridModule,
+        NzLayoutModule,
+        NzBreadCrumbModule,
+        NzDropDownModule,
+        NzMenuModule,
+        NzAvatarModule
+    ],
+    providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
